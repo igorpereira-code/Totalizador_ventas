f5a332898f8db09368011de82d583eda211fa5f5
Igorito27 <pereirarosadoigor@gmail.com>
2026-09-03 19:36:03 -0400

    feat: Implementando index.html y presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,8 @@
-import sumar from "./sumador";
+//import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#Totalizador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
@@ -11,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + calcularTotal(firstNumber, secondNumber) + "</p>";
 });
