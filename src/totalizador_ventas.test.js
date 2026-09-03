e16e723703d2335f77c94b350a929956ffd0f72d
Igorito27 <pereirarosadoigor@gmail.com>
2026-09-03 19:44:59 -0400

    feat: Si el precio por item es negativo o no numérico, se muestra un mensaje de error

--- src/totalizador_ventas.test.js
+++ src/totalizador_ventas.test.js
@@ -8,11 +8,20 @@ describe("Ventas", () => {
       "Error: La cantidad de items debe ser mayor que 0",
     );
   });
+
+  //Si el precio por item es negativo o no numérico, se muestra un mensaje de error.
+  it("El usuario ingresa precio por item negativo o no numérico → se muestra un mensaje de error y no se calcula el total", () => {
+    expect(calcularTotal(3, -2)).toEqual(
+      "Error: El precio por item debe ser un número mayor o igual a 0",
+    );
+  });
 });
 
 function calcularTotal(cantidad, precio) {
   if (cantidad <= 0) {
     return "Error: La cantidad de items debe ser mayor que 0";
+  } else if (precio < 0 || isNaN(precio)) {
+    return "Error: El precio por item debe ser un número mayor o igual a 0";
   }
   return cantidad * precio;
 }
